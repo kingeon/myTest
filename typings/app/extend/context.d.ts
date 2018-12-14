cc6076a909a2b915817ffac98b49f68ea96d006f
wangyu <yu.wang@icoastline.com>
2018-12-14 11:42:45 +0800

    提交更改

--- typings/app/extend/context.d.ts
+++
@@ -1,9 +0,0 @@
-// This file was auto created by egg-ts-helper
-// Do not modify this file!!!!!!!!!
-
-import 'egg'; // Make sure ts to import egg declaration at first
-import ExtendContext from '../../../app/extend/context';
-declare module 'egg' {
-  type ExtendContextType = typeof ExtendContext;
-  interface Context extends ExtendContextType { }
-}
--- typings/config/plugin.d.ts
+++
@@ -1,6 +0,0 @@
-// This file was auto created by egg-ts-helper
-// Do not modify this file!!!!!!!!!
-
-import 'egg'; // Make sure ts to import egg declaration at first
-import 'egg-validate';
-import 'egg-cors';
